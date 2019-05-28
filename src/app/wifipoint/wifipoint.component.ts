e4db9d5997dc7b9c0205b251bd154b861842b1b1
adrienn timko <adrienn.timko@gmail.com>
2019-05-28 13:30:55 +0200

    markers function created, to fix map

--- src/app/wifipoint/wifipoint.component.ts
+++ src/app/wifipoint/wifipoint.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { NgForm } from '@angular/forms';
 import * as mapboxgl from 'mapbox-gl';
-import { environment } from '../../environments/environment';
+import { environment } from "../../environments/environment";
 
 @Component({
   selector: 'app-wifipoint',
@@ -13,10 +13,11 @@ export class WifipointComponent implements OnInit {
    searchterm = ''; // to catch the input value
    apiData: any [];
    map: any;
+   marker: any;
   constructor(private apidata: DataService) { }
 
   ngOnInit() {
-
+   
     mapboxgl.accessToken = 'pk.eyJ1IjoiaGlyc2NoYmF1bSIsImEiOiJjanRlNng2b3EwazMyNDVxaThnb2MxOGtoIn0.2ZK2MPVV9Zoq_-EBVrafLg'
     this.getLocation();
   }
@@ -31,11 +32,23 @@ export class WifipointComponent implements OnInit {
       this.apiData = data.records;
       console.log(data.records[0].geometry.coordinates);
       this.showMap(data.records[0].geometry.coordinates[1], data.records[0].geometry.coordinates[0]);
-      }
+      this.apiData.forEach(function(marker){
+        var el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundImage = 'url(https://image.flaticon.com/icons/svg/149/149060.svg)';
+        
+        // add marker to map
+        new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .addTo('map');
+      });
+      
+    }
     });
   }
-    getWifisToTheMap() {
 
+    getMarkersToTheMap() {
+      //function here and this.blabla = data.records (from the API)
     }
 
     getLocation() {
@@ -53,5 +66,22 @@ export class WifipointComponent implements OnInit {
         zoom: 13
         });
     }
+
+    /*geojson.features.forEach(marker) {
+      // create a DOM element for the marker
+      var el = document.createElement('div');
+      el.className = 'marker';
+      el.style.backgroundImage = 'url(https://placekitten.com/g/' + marker.properties.iconSize.join('/') + '/)';
+      el.style.width = marker.properties.iconSize[0] + 'px';
+      el.style.height = marker.properties.iconSize[1] + 'px';
+       
+      el.addEventListener('click', function() {
+      window.alert(marker.properties.message);
+      });
+       
+      // add marker to map
+      new mapboxgl.Marker(el)
+      .setLngLat(marker.geometry.coordinates)
+      .addTo('map');*/
 
 }
